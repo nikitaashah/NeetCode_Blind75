d4fdd9fad576d768fb44512ba86277be576dda53
Nikita Shah <33662885+nikitaashah@users.noreply.github.com>
2024-12-26 21:28:40 +0530

    Create containsDuplicate.py

---
+++ Arrays_And_Hashing/Contains_Duplicate/containsDuplicate.py
@@ -0,0 +1,16 @@
+#####################################################################################################################################################
+#                                                                                                                                                   #
+# Problem Name : Contains Duplicate                                                                                                                 #
+# Level : Easy                                                                                                                                      #
+# Problem Statement: Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.             #
+#                                                                                                                                                   # 
+# Example 1:                                                                                                                                        #
+# Input: nums = [1, 2, 3, 3]                                                                                                                        #
+# Output: true                                                                                                                                      #
+#                                                                                                                                                   #
+# Example 2:                                                                                                                                        #
+# Input: nums = [1, 2, 3, 4]                                                                                                                        #
+# Output: false                                                                                                                                     #
+#                                                                                                                                                   #
+#####################################################################################################################################################
+
