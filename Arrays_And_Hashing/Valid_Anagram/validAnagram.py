aa0d5a3e8b0cf30364307fa6f3715de7e5e3e1da
Nikita Shah <33662885+nikitaashah@users.noreply.github.com>
2025-01-10 23:35:31 +0530

    Added my initial approach for Valid Anagram problem.

--- Arrays_And_Hashing/Valid_Anagram/validAnagram.py
+++ Arrays_And_Hashing/Valid_Anagram/validAnagram.py
@@ -1,1 +1,29 @@
+#####################################################################################################################################################
+#                                                                                                                                                   #
+# Problem Name : Valid Anagram                                                                                                                      #
+# Level : Easy                                                                                                                                      #
+# Problem Statement: Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.                  #
+#                                                                                                                                                   # 
+# Example 1:                                                                                                                                        #
+# Input: s = "racecar", t = "carrace"                                                                                                               #
+# Output: true                                                                                                                                      #
+#                                                                                                                                                   #
+# Example 2:                                                                                                                                        #
+# Input: s = "jar, t = "jam"                                                                                                                        #
+# Output: false                                                                                                                                     #
+#                                                                                                                                                   #
+# Medium article: https://nikitaashah.medium.com/arrays-and-hashing-valid-anagram-python-programming-solution-b98b0ee46540                          #
+#                                                                                                                                                   #
+#####################################################################################################################################################
 
+
+######################################
+#          Initial Approach          #
+######################################
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if sorted(s) == sorted(t):
+            return True
+        else:
+            return False
